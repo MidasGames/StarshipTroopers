92245fed19c7a38aee7f24131e06843c857ba3a1
Martinoxs <arcaya.martin@gmail.com>
2015-06-18 11:13:21 -0300

    rebote en pantalla de astronauta

--- public/javascripts/stage.js
+++ public/javascripts/stage.js
@@ -9,48 +9,88 @@ document.body.appendChild(renderer.view);
 requestAnimationFrame( animate );
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage("./images/lightning.png");
+var texture = PIXI.Texture.fromImage("./images/astronaut.png");
 // create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
+var astronauta = new PIXI.Sprite(texture);
 
 // center the sprites anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+astronauta.anchor.x = 0.5;
+astronauta.anchor.y = 0.5;
 
 // move the sprite t the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
+astronauta.position.x = 200;
+astronauta.position.y = 150;
 
-var sentido = 1;
+var sentidoX = 1;
+var sentidoY = 1;
 
-stage.addChild(bunny);
+stage.addChild(astronauta);
 
 function animate() {
 
     requestAnimationFrame( animate );
 
     // just for fun, lets rotate mr rabbit a little
-    bunny.rotation += 0.1;
-    //console.log(bunny.position);
-    if (bunny.position.x == renderer.view.width )
+    astronauta.rotation += 0.1;
+
+    //Movimiento en X
+    if (astronauta.position.x == renderer.view.width )
     {
-        sentido = 0;
+        sentidoX = 0;
     }
-    else if (bunny.position.x <= 0 )
+    else if (astronauta.position.x <= 0 )
     {
-        sentido = 1;
+        sentidoX = 1;
+    }
+
+    if (astronauta.position.x == renderer.view.width )
+    {
+        sentidoX = 0;
+    }
+    else if (astronauta.position.x <= 0 )
+    {
+        sentidoX = 1;
     }
 
 
-    if (sentido == 1)
+    if (sentidoX == 1)
     {
-        bunny.position.x += 5;
+        astronauta.position.x += 2;
     }
     else
     {
-        bunny.position.x -= 5;
+        astronauta.position.x -= 2;
     }
 
+
+    //Movimiento en Y
+    if (astronauta.position.y == renderer.view.height )
+    {
+        sentidoY = 0;
+    }
+    else if (astronauta.position.y <= 0 )
+    {
+        sentidoY = 1;
+    }
+
+    if (astronauta.position.y == renderer.view.height )
+    {
+        sentidoY = 0;
+    }
+    else if (astronauta.position.y <= 0 )
+    {
+        sentidoY = 1;
+    }
+
+
+    if (sentidoY == 1)
+    {
+        astronauta.position.y += 2;
+    }
+    else
+    {
+        astronauta.position.y -= 2;
+    }
 
 
     // render the stage

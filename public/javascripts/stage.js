6f9f2d42c687e7fd4f311ada3a5b6991696bfb33
Martinoxs <arcaya.martin@gmail.com>
2015-06-18 10:57:53 -0300

    rebote horizontal del rayito

--- public/javascripts/stage.js
+++ public/javascripts/stage.js
@@ -1,7 +1,7 @@
 var stage = new PIXI.Stage(0x66FF99);
 
 // create a renderer instance.
-var renderer = PIXI.autoDetectRenderer(400, 300);
+var renderer = PIXI.autoDetectRenderer(600, 400);
 
 // add the renderer view element to the DOM
 document.body.appendChild(renderer.view);
@@ -21,6 +21,8 @@ bunny.anchor.y = 0.5;
 bunny.position.x = 200;
 bunny.position.y = 150;
 
+var sentido = 1;
+
 stage.addChild(bunny);
 
 function animate() {
@@ -29,7 +31,27 @@ function animate() {
 
     // just for fun, lets rotate mr rabbit a little
     bunny.rotation += 0.1;
-    bunny.position.x += 1;
+    //console.log(bunny.position);
+    if (bunny.position.x == renderer.view.width )
+    {
+        sentido = 0;
+    }
+    else if (bunny.position.x <= 0 )
+    {
+        sentido = 1;
+    }
+
+
+    if (sentido == 1)
+    {
+        bunny.position.x += 5;
+    }
+    else
+    {
+        bunny.position.x -= 5;
+    }
+
+
 
     // render the stage
     renderer.render(stage);

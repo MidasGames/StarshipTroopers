15eb27513910cd93441d255b6c537aaa3e083521
SNoRKeL <snorkel84@gmail.com>
2015-06-18 15:22:55 -0300

    dvd

--- public/javascripts/stage.js
+++ public/javascripts/stage.js
@@ -9,75 +9,61 @@ document.body.appendChild(renderer.view);
 requestAnimationFrame( animate );
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage("./images/astronaut.png");
+var texture = PIXI.Texture.fromImage("./images/dvdLogo.png");
 // create a new Sprite using the texture
-var astronauta = new PIXI.Sprite(texture);
+var dvd = new PIXI.Sprite(texture);
 
 // center the sprites anchor point
-astronauta.anchor.x = 0.5;
-astronauta.anchor.y = 0.5;
+dvd.anchor.x = 0.5;
+dvd.anchor.y = 0.5;
 
 // move the sprite t the center of the screen
-astronauta.position.x = 200;
-astronauta.position.y = 150;
+dvd.position.x = 200;
+dvd.position.y = 150;
 
 var sentidoX = 1;
 var sentidoY = 1;
 
-stage.addChild(astronauta);
+
+stage.addChild(dvd);
 
 function animate() {
 
     requestAnimationFrame( animate );
 
     // just for fun, lets rotate mr rabbit a little
-    astronauta.rotation += 0.1;
+    if (sentidoX == 0 || sentidoY == 0)
+    {
+        dvd.tint = getRandomColor();
+    }
+
 
     //Movimiento en X
-    if (astronauta.position.x == renderer.view.width )
+    if (dvd.position.x == renderer.view.width )
     {
         sentidoX = 0;
     }
-    else if (astronauta.position.x <= 0 )
+    else if (dvd.position.x <= 0 )
     {
         sentidoX = 1;
     }
-
-    if (astronauta.position.x == renderer.view.width )
-    {
-        sentidoX = 0;
-    }
-    else if (astronauta.position.x <= 0 )
-    {
-        sentidoX = 1;
-    }
-
 
     if (sentidoX == 1)
     {
-        astronauta.position.x += 2;
+        dvd.position.x += 2;
     }
     else
     {
-        astronauta.position.x -= 2;
+        dvd.position.x -= 2;
     }
 
 
     //Movimiento en Y
-    if (astronauta.position.y == renderer.view.height )
+    if (dvd.position.y == renderer.view.height )
     {
         sentidoY = 0;
     }
-    else if (astronauta.position.y <= 0 )
-    {
-        sentidoY = 1;
-    }
-
-    if (astronauta.position.y == renderer.view.height )
-    {
-        sentidoY = 0;
-    }
-    else if (astronauta.position.y <= 0 )
+    else if (dvd.position.y <= 0 )
     {
         sentidoY = 1;
     }
@@ -85,13 +71,22 @@ function animate() {
 
     if (sentidoY == 1)
     {
-        astronauta.position.y += 2;
+        dvd.position.y += 2;
     }
     else
     {
-        astronauta.position.y -= 2;
+        dvd.position.y -= 2;
     }
 
     // render the stage
     renderer.render(stage);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '0x';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
